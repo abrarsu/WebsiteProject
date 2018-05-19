282742a29c7fed4ed76227a9147bd3af2dc145d8
Chuckles\Abrooo <abrar.suliman@students.plymouth.ac.uk>
2018-05-19 10:07:14 +0100

    sholing cart not complete yet

---
+++ JQueryAjaxInMVC2/JQueryAjaxInMVC2/Controllers/Item.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JQueryAjaxInMVC2.Models;
+namespace JQueryAjaxInMVC2.Controllers
+{
+    public class Item
+    {
+        private CustomerBooking booking = new CustomerBooking();
+
+        public CustomerBooking Booking
+        {
+            get
+            {
+                return booking;
+            }
+
+            set
+            {
+                booking = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        private int quantity;
+
+        public Item()
+        {
+
+        }
+
+        public Item(CustomerBooking booking, int quantity)
+        {
+            this.Booking = booking;
+            this.Quantity = quantity;
+        }
+
+      
+    }
+}
---
+++ JQueryAjaxInMVC2/JQueryAjaxInMVC2/Controllers/ShoppingCartController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JQueryAjaxInMVC2.Models;
+
+namespace JQueryAjaxInMVC2.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        DBModel db = new DBModel();
+
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BookNow(int id)
+        {
+            if(Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                cart.Add(new Item(db.CustomerBookings.Find(id), 1));
+                Session["cart"] = cart;
+            }
+            else
+            {
+
+            }
+            return View("Cart");
+        }
+    }
+}
