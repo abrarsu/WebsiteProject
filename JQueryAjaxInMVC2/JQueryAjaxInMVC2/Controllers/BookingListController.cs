4e5128d0b52309d788367158d777cf8d08d0ca95
Chuckles\Abrooo <abrar.suliman@students.plymouth.ac.uk>
2018-05-21 17:59:55 +0100

    booking list view done for admin side

---
+++ JQueryAjaxInMVC2/JQueryAjaxInMVC2/Controllers/BookingListController.cs
@@ -0,0 +1,58 @@
+﻿using JQueryAjaxInMVC2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQueryAjaxInMVC2.Controllers
+{
+    public class BookingListController : Controller
+    {
+        // GET: BookingList
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewBookingList()
+        {
+            return View(GetAllBookings());
+        }
+
+        public IEnumerable<BookingViewModel> GetAllBookings()
+        {
+            using (DBModel db = new DBModel())
+            {
+                IEnumerable<CustomerBooking> customerBookings = db.CustomerBookings.ToList();
+
+                IEnumerable<BookingViewModel> bookingList = customerBookings.Select(x => new BookingViewModel
+                {
+                    BookingID = x.BookingID,
+                    ClassID = x.ClassID,
+                    ClubID = x.Class.ClubID,
+                    ClubName = x.Class.Club.ClubName,
+                    ClassDate = x.Class.ClassDate,
+                    ClassTime = x.Class.ClassTime,
+                    InstructorID = x.Class.InstructorID,
+                    instrFirstName = x.Class.Instructor.FirstName,
+                    instrLastName = x.Class.Instructor.LastName,
+                    CustomerID = x.CustomerID,
+                    FirstName = x.Customer.FirstName,
+                    LastName = x.Customer.LastName,
+                    BeltLevel = x.Customer.GradeBelt.BeltLevel,
+                    BeltLevelColour = x.Customer.GradeBelt.BeltLevelColour,
+                    ClassGIAGPrice = x.Class.ClassGIAGPrice,
+                    VenueName = x.Class.VenueName,
+                    AddressLine1 = x.Class.AddressLine1,
+                    AddressLine2 = x.Class.AddressLine2,
+                    Postcode = x.Class.Postcode
+
+
+                }).ToList();
+
+                return bookingList;
+            }
+        }
+    }
+}
--- JQueryAjaxInMVC2/JQueryAjaxInMVC2/Models/BookingViewModel.cs
+++ JQueryAjaxInMVC2/JQueryAjaxInMVC2/Models/BookingViewModel.cs
@@ -18,10 +18,40 @@ namespace JQueryAjaxInMVC2.Models
         [DisplayName("Class Name")]
         public string ClubName { get; set; }
 
+        public System.DateTime ClassDate { get; set; }
+
+        public string ClassTime { get; set; }
+
+
         [DisplayName("Customer Name")]
         public string FirstName { get; set; }
 
         [DisplayName("Customer Name")]
         public string LastName { get; set; }
+
+        public int BeltLevel { get; set; }
+
+        public string instrFirstName { get; set; }
+
+        public string instrLastName { get; set; }
+
+        public int InstructorID { get; set; }
+
+        public Nullable<decimal> ClassGIAGPrice { get; set; }
+
+        public string BeltLevelColour { get; set; }
+
+        [DisplayName("Venue Name")]
+        public string VenueName { get; set; }
+
+        [DisplayName(" Address Line 1")]
+        public string AddressLine1 { get; set; }
+
+        [DisplayName(" Address Line 2")]
+        public string AddressLine2 { get; set; }
+        public string Postcode { get; set; }
+
+
+
     }
 }
